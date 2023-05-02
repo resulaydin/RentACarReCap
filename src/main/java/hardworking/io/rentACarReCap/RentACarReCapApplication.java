7daf6893b6ac3a6099b3ea9f168c0a812b6b5e09
resul aydın <bm.resulaydin@gmail.com>
2023-05-02 23:06:24 +0300

    entities-dataAccess-business-Mapping-controller processes have performed

--- src/main/java/hardworking/io/rentACarReCap/RentACarReCapApplication.java
+++ src/main/java/hardworking/io/rentACarReCap/RentACarReCapApplication.java
@@ -1,13 +1,23 @@
 package hardworking.io.rentACarReCap;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @SpringBootApplication
+@RestControllerAdvice
 public class RentACarReCapApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RentACarReCapApplication.class, args);
 	}
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+	
 
 }
