a9d4bc34b303819ad660ad2e5b5b0c5c0d04a131
resul aydın <bm.resulaydin@gmail.com>
2023-05-02 13:46:16 +0300

    rentACarReCap project has started

---
+++ src/main/java/hardworking/io/rentACarReCap/RentACarReCapApplication.java
@@ -0,0 +1,13 @@
+package hardworking.io.rentACarReCap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentACarReCapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentACarReCapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hardworking/io/rentACarReCap/RentACarReCapApplicationTests.java
@@ -0,0 +1,13 @@
+package hardworking.io.rentACarReCap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarReCapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
